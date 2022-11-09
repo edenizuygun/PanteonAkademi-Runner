cbd6602710e48d4dcf2d8260ebc974ddb9785b48
edenizuygun <esma.deniz.uygun@gmail.com>
2022-11-09 21:16:45 +0300

    runner project

--- RunnerPanteonAkademi/Assets/Scripts/CollectCoin.cs
+++ RunnerPanteonAkademi/Assets/Scripts/CollectCoin.cs
@@ -9,6 +9,17 @@ public class CollectCoin : MonoBehaviour
     public int score;
     public TextMeshProUGUI coinText;
     public PlayerController playerController;
+    public int maxScore;
+
+    public Animator playerAnim;
+    public GameObject player;
+
+    public GameObject endPanel;
+
+    private void Start()
+    {
+        playerAnim = player.GetComponentInChildren<Animator>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
@@ -18,9 +29,31 @@ public class CollectCoin : MonoBehaviour
         }
         else if (other.CompareTag("End"))
         {
-            Debug.Log("congrats");
+           // Debug.Log("congrats");
             playerController.runningSpeed = 0f;
+            transform.Rotate(transform.rotation.x,180f, transform.rotation.z,Space.Self);
+            endPanel.SetActive(true);
+            if (score >= maxScore)
+            {
+               // Debug.Log("win");
+                playerAnim.SetBool("Win",true);
+                
+            }
+            else
+            {
+               // Debug.Log("lose");
+                playerAnim.SetBool("Lose", true);
+                
+            }
         }
+    }
+
+    public void RestartBtn() {
+
+
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
     private void OnCollisionEnter(Collision collision)
     {
